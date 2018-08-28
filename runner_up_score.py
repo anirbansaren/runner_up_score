2f055f8eeb78c95dc81e147c27c8eba6bc5ba7c2
anirbansaren <anirbansaren@gmail.com>
2018-08-28 21:21:17 +0530

    changes using sort function

--- runner_up_score.py
+++ runner_up_score.py
@@ -1,12 +1,12 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    arr1=list(arr)
-    length=len(arr1)
+    arr1 = list(arr)
+    length = len(arr1)
     arr1.sort()
-    i=length-1
-    while i>0:
-        if arr1[i]<arr1[-1]:
+    i = length - 1
+    while i >= 0:
+        if arr1[i] < arr1[-1]:
             print(arr1[i])
             break
-        i-=1
+        i -= 1
