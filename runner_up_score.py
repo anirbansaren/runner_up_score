6f738da902a1cca53f6c0ff613413d4202b74311
anirbansaren <anirbansaren@gmail.com>
2018-08-28 20:49:27 +0530

    changes

--- runner_up_score.py
+++ runner_up_score.py
@@ -1,16 +1,14 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    arr1=arr
-    max_num=-100
-    second_max=-100
+    arr1 = arr
+    max_num = -100
+    second_max = -100
     for i in arr1:
-        if i>max_num:
-            if second_max<max_num:
-                second_max=max_num
+        if i > max_num:
+            if second_max < max_num:
+                second_max = max_num
             else:
-                max_num=i
-        else:
-            max_num=i
+                max_num = i
 
     print(second_max)
